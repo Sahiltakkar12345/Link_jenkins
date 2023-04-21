96375b80a5d629bb7162d3bfb0f2df698db288ce
Sahiltakkar12345 <Sahiltakkar12345@gmail.com>
2023-04-21 10:54:26 +0530

    localupdate

--- Demo.java
+++ Demo.java
@@ -4,6 +4,6 @@ public static void main(String[] args)
 {
 System.out.println("this changes done in github");
 System.out.println("this is the second program");
-
+System.out.println("this changes done in local host");
 }
 }
